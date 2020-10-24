88504049cea48e0f3e67584c8f7a7f9c2c22931e
Vito Atmo <bhadrika25@gmail.com>
2020-10-24 17:27:28 +0700

    not all done

---
+++ _6_encyption_transpos_cipher.py
@@ -0,0 +1,23 @@
+import pyperclip
+
+def main():
+    myMessage = input("Input : ")
+    myKey = int(input("Key : "))
+    ciphertext = encryptMessage(myKey, myMessage)
+    print("Cipher Text is")
+    print(ciphertext + '|')
+    pyperclip.copy(ciphertext)
+
+def encryptMessage(key, message):
+    ciphertext = [''] * key
+    
+    for col in range(key):
+        position = col
+
+        while position < len(message):
+            ciphertext[col] += message[position]
+            position += key
+    return ''.join(ciphertext)  # Cipher text
+
+if __name__ == '__main__':
+    main()
