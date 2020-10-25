eeae58355d03248351b3afb439a78127b92e0eee
Vito Atmo <bhadrika25@gmail.com>
2020-10-25 16:08:10 +0700

    a

--- _1_reverse_cipher.py
+++ _1_reverse_cipher.py
@@ -1,7 +1,8 @@
-message = 'This is program to explain reverse cipher.'
+message = input('Input: ')
 translated = '' #cipher text is stored in this variable
 i = len(message) - 1
 while i >= 0:
     translated = translated + message[i]
     i = i - 1
+print('The message text is : ', message)
 print('The cipher text is : ', translated)
